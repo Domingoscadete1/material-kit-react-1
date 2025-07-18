9807ec409dc5fa5f854f3877c249c2eab0f8b18f
DomingosCadete1 <domingoscadetez66@gmail.com>
2025-07-18 12:41:04 +0100

    ss

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 export default class Config {
-    static API_URL = 'https://c3e5c9c391c7.ngrok-free.app/';
+    static API_URL = 'https://042d53b562dc.ngrok-free.app/';
     static APP_NAME = 'MeuApp';
-    static API_URL_WS = 'c3e5c9c391c7.ngrok-free.app';
-    static API_MEDIA_URL = 'https://c3e5c9c391c7.ngrok-free.app';
+    static API_URL_WS = '042d53b562dc.ngrok-free.app';
+    static API_MEDIA_URL = 'https://042d53b562dc.ngrok-free.app';
   
     static getApiUrlMedia() {
       return this.API_MEDIA_URL;
